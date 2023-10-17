143fbcd6ce0d52205e51f666581d4c3ade59edc0
URIAN VIERA <urian1213viera@gmail.com>
2023-10-16 20:14:41 -0500

    rutas

--- data_xlsx/urls.py
+++ data_xlsx/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
-    path('subir-data-xlsx', views.cargar_archivo, name="cargar_archivo"),
+    path('subir-data-xlsx/', views.cargar_archivo, name="cargar_archivo"),
 
     path('data-demo/', views.data_demo, name='data_demo'),
 ]
